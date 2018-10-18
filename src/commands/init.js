9acaef7aea22e1f78b5bf7ede55f6f71ec77b9dd
Christian Torres <christiant@rain.agency>
2018-10-18 17:08:30 -0600

    Starting v2 of voxa-cli

---
+++ src/commands/init.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(argv) {
+  
+};
+
+module.exports.description = 'generate a new project';
+module.exports.alias = 'i';
---
+++ src/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const commander = require('commander');
+
+module.exports = function(argv) {
+  const pkg = require('../package.json');
+
+  commander.version(pkg.version);
+  let description = 'Run a generator. Type can be\n';
+
+
+  [
+    'interaction',
+    'init',
+  ].forEach(c => {
+    commander.usage(c);
+
+    const command = require(`./commands/${c}`)
+    const { alias, description } = command;
+    commander.command(c)
+      .alias(alias)
+      .description(description)
+      .action(command);
+  });
+
+  commander.parse(argv);
+  
+  if (argv.length === 2) {
+    commander.help();
+  }
+};
