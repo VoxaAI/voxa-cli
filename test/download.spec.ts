68aa15c3d16b33d0de980d86ff2119f86a7d1ff1
Rommel Berrios <rommel.berrios93@gmail.com>
2019-03-22 16:41:06 -0600

    t push origin staging --force add test to travis

--- test/download.spec.ts
+++ test/download.spec.ts
@@ -6,17 +6,21 @@ import { hasAGoogleServiceAccount } from "./utils";
 use(Chaifs);
 
 describe("Media Assets", async () => {
+  before(async function() {
+    if (await hasAGoogleServiceAccount()) {
+      this.skip();
+    }
+  });
+
   it("should create the assets directory", () => {
     expect(path.join(__dirname, "out/assets")).to.exist;
   });
 
-  if (await hasAGoogleServiceAccount()) {
-    it("should download the files and those be valid", () => {
-      const originalImage = path.join(__dirname, "out/assets/images/small.png");
-      const downloadedImage = path.join(__dirname, "assets/images/small.png");
-      expect(originalImage)
-        .to.be.a.file()
-        .and.equal(downloadedImage);
-    });
-  }
+  it("should download the files and those be valid", () => {
+    const originalImage = path.join(__dirname, "out/assets/images/small.png");
+    const downloadedImage = path.join(__dirname, "assets/images/small.png");
+    expect(originalImage)
+      .to.be.a.file()
+      .and.equal(downloadedImage);
+  });
 });
