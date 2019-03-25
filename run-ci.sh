336d06ec9c59c3b53824835e2b42d7c1d243933f
Rommel Berrios <rommel.berrios93@gmail.com>
2019-03-25 11:37:51 -0600

    update run-ci test

--- run-ci.sh
+++ run-ci.sh
@@ -6,9 +6,6 @@ rm -rf test/out
 
 yarn install --frozen-lockfile
 yarn lint
-yarn test
 
-if [ "${TRAVIS_SECURE_ENV_VARS:-}" = "true" ]; then
-  yarn test-ci
-  yarn report
-fi
+yarn test-ci
+yarn report
