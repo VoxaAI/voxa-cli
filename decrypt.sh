df3e89ed70d17f83cf6e66be14d6e3414a7d50f1
Andrés Reyes Monge <armonge@gmail.com>
2019-05-20 20:42:40 +0200

    Forgot to add untar

--- decrypt.sh
+++ decrypt.sh
@@ -4,4 +4,5 @@ set -o xtrace
 
 if [ "${TRAVIS_SECURE_ENV_VARS:-}" = "true" ]; then
   openssl aes-256-cbc -K $encrypted_25878a959762_key -iv $encrypted_25878a959762_iv -in secrets.tar.enc -out secrets.tar -d
+  tar xvf secrets.tar
 fi
