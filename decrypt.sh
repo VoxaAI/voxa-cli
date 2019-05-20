7d40605302b818f1d05e6bda8d41049a33247f4c
Andrés Reyes Monge <armonge@gmail.com>
2019-05-20 20:28:33 +0200

    Adding azure_secret

--- decrypt.sh
+++ decrypt.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 set -o xtrace
 
 if [ "${TRAVIS_SECURE_ENV_VARS:-}" = "true" ]; then
-  openssl aes-256-cbc -K $encrypted_25878a959762_key -iv $encrypted_25878a959762_iv \
-    -in client_secret.json.enc -out test/client_secret.json -d
-  openssl aes-256-cbc -K $encrypted_25878a959762_key -iv $encrypted_25878a959762_iv \
-    -in azure_secret.json.enc -out test/azure_secret.json -d
+  openssl aes-256-cbc -K $encrypted_25878a959762_key -iv $encrypted_25878a959762_iv -in client_secret.json.enc -out test/client_secret.json -d
+  openssl aes-256-cbc -K $encrypted_25878a959762_key -iv $encrypted_25878a959762_iv -in azure_secret.json.enc -out test/azure_secret.json -d
 fi
