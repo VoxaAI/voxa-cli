d2f1e50cb8e84a1f2804d4609324ddce77811a2e
Rommel Berrios <rommel.berrios93@gmail.com>
2019-12-18 12:23:59 -0600

    fixess #98 - use local.example by default

--- templates/javascript/src/config/index.js
+++ templates/javascript/src/config/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const path = require("path");
-const env = process.env.NODE_ENV || "local";
+const env = process.env.NODE_ENV || "local.example";
 
 const configFile = require(path.join(__dirname, `${env}.json`));
 configFile.env = env;
